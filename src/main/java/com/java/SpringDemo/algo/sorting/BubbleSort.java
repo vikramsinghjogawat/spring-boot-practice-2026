6402eb12275a064231e56488a5abb2a262952ca2
Vikram Singh <vikramsinghjogawat5@gmail.com>
2026-06-25 15:14:00 +0530

    Bubble sort Algo refined

--- src/main/java/com/java/SpringDemo/algo/sorting/BubbleSort.java
+++ src/main/java/com/java/SpringDemo/algo/sorting/BubbleSort.java
@@ -1,27 +1,35 @@
 package com.java.SpringDemo.algo.sorting;
 
 public class BubbleSort {
-    public static void main(String[] args) {
-        //Bubble sort
-        int[] nums = {92,53,24,74,46};
-        int l=0;
-        for (int j = 0; j < nums.length-1; j++) {
-            for (int i = 0; i < nums.length -l- 1; i++) {
-                System.out.println(nums[i] + "," + nums[i + 1]);
-                if(nums[i]>nums[i+1]){
-                    swap(i,i+1,nums);
-                }
+        public static void main(String[] args) {
+            //Bubble sort
+            int[] nums = {92,53,24,74,46};
+            bubbleSort(nums);
+            System.out.println("==Sorted Array==");
+            for(int i: nums){
+                System.out.print(i+",");
             }
-            l++;
-            System.out.println("----------------------------------------------------");
         }
-        for(int i:nums){
-            System.out.println(i);
+        public static void swap(int i,int j,int[] arr){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
-    }
-    public static void swap(int i,int j,int[] arr){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+        public static void bubbleSort(int[] nums){
+            int m=nums.length-2;
+            while(m>= 0) {
+                int e = 0;
+                int f = 1;
+                while (e <= m) {
+                    //printing series of indices
+                    //System.out.println(e + "," + f);
+                    if(nums[f]<nums[e]){
+                        swap(e,f,nums);
+                    }
+                    e++;
+                    f++;
+                }
+                m--;
+            }
+        }
 }
