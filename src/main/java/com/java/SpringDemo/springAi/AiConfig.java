795d5433487fc5cdd5555cb2e69942e6f069c49a
Vikram Singh <vikramsinghjogawat5@gmail.com>
2026-06-26 14:20:19 +0530

    Added Spring AI chatClient code and props

---
+++ src/main/java/com/java/SpringDemo/springAi/AiConfig.java
@@ -0,0 +1,14 @@
+package com.java.SpringDemo.springAi;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AiConfig {
+    @Bean
+    ChatClient chatClient(ChatClient.Builder builder){
+        return builder.defaultAdvisors(new SimpleLoggerAdvisor()).build();
+    }
+}
